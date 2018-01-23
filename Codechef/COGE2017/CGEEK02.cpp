bc20e29885322e49d02288ce827c0b5c1067b4fd
rsenwar <rsenwar448@gmail.com>
2018-01-23 22:08:22 +0530

    Create CGEEK02.cpp

---
+++ Codechef/COGE2017/CGEEK02.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+ string s;
+ int t;
+ cin>>t;
+ while(t--){
+  cin>>s;
+  int len = s.length();
+  int c = 1;
+  char ini = s[0];
+  for(int i=1;i<len;i++)
+   if(s[i]!=ini){
+    ini = s[i];c++;}
+  cout<<c/2<<endl;
+ }
+return 0;
+}
